b248a6aad135311c3b93ea7fce26f963f1af9f92
Nicolas Reyland <nicolas.reyland@epita.fr>
2021-07-18 20:44:37 +0200

    basic ll structure

--- linked_list.c
+++ linked_list.c
@@ -1,2 +1,140 @@
-
+/* LinkedList Implementation */
+#include "linked_list.h"
+#include <stdio.h>
 #include <errno.h>
+
+// error message
+void _exit_msg(const char* msg)
+{
+	fprintf(stderr, msg);
+	fprintf(stderr, "\n");
+	exit(EXIT_FAILURE);
+}
+
+// initialize the list
+void _init_list(LinkedList *list, void* value)
+{
+	list->length = 1;
+	list->value_size = sizeof(value);
+	list->head = _new_node();
+	list->head->value = value;
+	list->head->next = NULL;
+	list->_initialized = true;
+}
+
+// is an index valid ?
+bool _valid_index(LinkedList *list, int index, bool include_end)
+{
+	return 0 <= index && index < list->length + include_end;
+}
+
+// assert that an index is valid
+void _assert_index(LinkedList *list, int index, bool include_end)
+{
+	if (_valid_index(list, index, include_end)) return;
+	char* msg = LL_INDEX_ERROR_MSG;
+	sprintf(msg, index, list->length);
+	_exit_msg(msg);
+}
+
+// get the value at the index
+Node* _get_node_at(LinkedList *list, int index)
+{
+	Node* node = list->head;
+
+	for (int i = 0; i < index; i++)
+	{
+		node = node->next;
+		// verify after we get the next node: the head should have been verified in the _assert_index call
+		if (node == NULL)
+		{
+			char* msg = LL_NULL_POINTER_NODE;
+			sprintf(msg, i);
+			_exit_msg(msg);
+		}
+	}
+
+	return node;
+}
+
+// create a new node
+Node* _new_node()
+{
+	// allocate new memory for node
+	size_t node_size = sizeof(struct Node);
+	struct Node *node = (struct Node*) malloc(node_size);
+	// if the pointer is NULL, the allocation faled
+	if (node == NULL)
+	{
+		char* msg = LL_MEM_ALLOC_ERROR_MSG;
+		sprintf(msg, node_size);
+		_exit_msg(msg);
+	}
+	return node;
+}
+
+
+/* 'Public' Methods */
+
+// get value at index
+void* get_value_at(LinkedList *list, int index)
+{
+	_assert_index(list, index, false);
+	return _get_node_at(list, index)->value;
+}
+
+// insert value at index
+void insert_value_at(LinkedList *list, void* value, int index)
+{
+	// init list ?
+	if (index == 0)
+	{
+		if (!list->_initialized)
+		{
+			_init_list(list, value);
+			return;
+		}
+		// ...
+	}
+	_assert_index(list, index, true);
+}
+
+// remove value at index
+void* remove_value_at(LinkedList *list, int index)
+{
+	_assert_index(list, index, false);
+}
+
+// remove value
+int remove_value(LinkedList *list, void* value)
+{
+	return 0;
+}
+
+// append value
+void append_value(LinkedList *list, void* value)
+{
+	// init list ?
+	if (!list->_initialized)
+	{
+		_init_list(list, value);
+		return;
+	}
+
+	// add to empty list
+	if (list->length == 0)
+	{
+		list->head = _new_node();
+		list->head->value = value;
+	}
+	// add to tail of the list
+	else
+	{
+		Node *tail = _get_node_at(list, list->length - 1);
+		tail->next = _new_node();
+		tail->next->value = value;
+	}
+
+	list->length++;
+}
+
--- linked_list.h
+++ linked_list.h
@@ -1,6 +1,15 @@
-/* LinkedList Implementation */
+/* LinkedList Declaration */
 #include <stdlib.h>
 #include <stdbool.h>
+
+/* Constants */
+#define LL_INDEX_ERROR_MSG "linked_list:IndexError: %d out of bounds with linked list of length %d"
+#define LL_MEM_ALLOC_ERROR_MSG "linked_list:AllocationError: Could not allocate memory of size: %d"
+#define LL_ACESS_EMPTY_LIST "linked_list:Tried to access an empty linked list"
+#define LL_NULL_POINTER_NODE "linked_list:Node at index: %d the list is NULL"
+
+
+/* Structures */
 
 // Node structure
 typedef struct Node
@@ -12,26 +21,29 @@ typedef struct Node
 // Linked List structure
 typedef struct LinkedList
 {
-	bool _initialized;
 	int length;
-	size_t element_size;
+	size_t value_size;
 	Node *head;
+	bool _initialized;
 } LinkedList;
 
 
 /* 'Private' Methods */
 
+// error message
+void _exit_msg(const char* msg);
+
 // initialize the list
-void _init_list(LinkedList *list, void* element);
+void _init_list(LinkedList *list, void* value);
 
 // is an index valid ?
-bool _valid_index(LinkedList *list, int index);
+bool _valid_index(LinkedList *list, int index, bool include_end);
 
 // assert that an index is valid
-void _assert_index(LinkedList *list, int index);
+void _assert_index(LinkedList *list, int index, bool include_end);
 
-// get the element at the index
-Node* _get_element_at(LinkedList *list, int index);
+// get the node at the index
+Node* _get_node_at(LinkedList *list, int index);
 
 // create a new node
 Node* _new_node();
@@ -39,17 +51,17 @@ Node* _new_node();
 
 /* 'Public' Methods */
 
-// get element at index
-void* get_element_at(LinkedList *list, int index);
+// get value at index
+void* get_value_at(LinkedList *list, int index);
 
-// insert element at index
-void insert_element_at(LinkedList *list, void* element, int index);
+// insert value at index
+void insert_value_at(LinkedList *list, void* value, int index);
 
-// remove element at index
-void* remove_element_at(LinkedList *list, int index);
+// remove value at index
+void* remove_value_at(LinkedList *list, int index);
 
-// remove element
-int remove_element(LinkedList *list, void* element);
+// remove value
+int remove_value(LinkedList *list, void* value);
 
 // append value
-void append_element(LinkedList *list, void* element);
+void append_value(LinkedList *list, void* value);
